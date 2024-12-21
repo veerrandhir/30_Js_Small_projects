1e307c854e3831e4a226e6f6169474261e7fa6d2
veerrandhir <veerrandhir3@gmail.com>
2024-12-21 16:23:10 +0530

    new changes

---
+++ 04_KeyTable/veercode.js
@@ -0,0 +1,21 @@
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+  insert.innerHtml = `
+    <div class='color'>
+    <table>
+    <tr>
+      <th>Key</th>
+      <th>Key Code</th>
+      <th>Code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'space' : e.key}</td>
+      <td>${e.keyCode}</td>
+      <td>${e.code}</td>
+    </tr>
+
+  </table>
+     </div>
+    `;
+});
