e9e43b352aa2fc5c3cb1c26197ab6c0cb7aefbca
veerrandhir <veerrandhir3@gmail.com>
2025-01-12 18:48:02 +0530

    Website ready Api my misbehave

--- 15_ApiCat&Dog/index.js
+++ 15_ApiCat&Dog/index.js
@@ -3,15 +3,23 @@ const dog_result = document.getElementById("dog_result")
 const cat_button = document.getElementById("cat_button")
 const dog_button = document.getElementById("dog_button")
 
-function catClick() {cat_button.addEventListener("click", ()=>{
-    cat_result.innerText = "Hello I am a cat"
-    console.log("Cat was clicked");
-    
-})}
-catClick()
-function dogClick() {dog_button.addEventListener("click", ()=>{
-    dog_result.innerText = "Hello I am a dog"
-    console.log("Dog was clicked");
-    
-})}
-dogClick()
+
+cat_button.addEventListener('click',getRandomCat)
+dog_button.addEventListener('click',getRandomDog)
+
+function  getRandomCat(){
+    fetch('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME')
+    .then(res => res.json())
+    .then(data =>{
+        cat_result.innerHTML = `<img 
+        src="${data.url}"/>`
+    })
+} 
+function  getRandomDog(){
+    fetch('https://dog.ceo/api/breeds/image/random') // Api key is not working so unable to get image but everything is working fine
+    .then(res => res.json())
+    .then(data =>{
+        dog_result.innerHTML = `<img 
+        src="${data.url}"/>`
+    })
+} 
